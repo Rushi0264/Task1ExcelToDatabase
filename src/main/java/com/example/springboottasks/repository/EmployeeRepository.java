3069cfdbcdcc34028e28a498aced69f19b02ce25
Rushi0264 <rushikeshchepte2@gmail.com>
2026-06-16 15:46:30 +0530

    excel to database

---
+++ src/main/java/com/example/springboottasks/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.springboottasks.repository;
+
+import com.example.springboottasks.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ src/test/java/com/example/springboottasks/SpringboottasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
